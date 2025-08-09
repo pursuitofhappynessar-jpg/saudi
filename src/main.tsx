9bb79c82950ec499c033c5bbcfee955422ac202e
pursuitofhappynessar-jpg <pursuit.of.happyness.ar@gmail.com>
2025-08-09 17:39:20 +0300

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import emailjs from '@emailjs/browser';
 import App from './App.tsx';
 import './index.css';
+
+// Initialize EmailJS
+emailjs.init("");
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
